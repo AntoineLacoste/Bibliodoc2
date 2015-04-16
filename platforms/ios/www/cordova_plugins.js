bc5b981573b1b184e240a661578ee1d8f0b693d6
AntoineLacoste <antoine.lacoste@iut-valence.fr>
2015-04-16 15:52:47 +0200

    TestPlugin indexed DB shim

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,16 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.msopentech.indexedDB/www/IndexedDBShim.min.js",
+        "id": "com.msopentech.indexedDB.IndexedDBShim",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.msopentech.indexedDB": "0.1.1",
+    "com.msopentech.websql": "0.0.7"
+}
+// BOTTOM OF METADATA
+});
