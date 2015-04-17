657607603e87f466f55b5561a9dec7294e313748
AntoineLacoste <antoine.lacoste@iut-valence.fr>
2015-04-17 09:48:44 +0200

    test ios 6

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,15 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.dvdbrink.cordova.indexeddb/www/IndexedDBShim.js",
+        "id": "com.dvdbrink.cordova.indexeddb.indexeddb",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.dvdbrink.cordova.indexeddb": "0.1.0"
+}
+// BOTTOM OF METADATA
+});
