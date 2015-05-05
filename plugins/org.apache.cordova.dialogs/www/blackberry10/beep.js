6c5c02945b12126a297a00b1308cd329d68e1d13
AntoineLacoste <antoine.lacoste@iut-valence.fr>
2015-05-05 16:03:52 +0200

    DR 4

--- plugins/org.apache.cordova.dialogs/www/blackberry10/beep.js
+++ plugins/org.apache.cordova.dialogs/www/blackberry10/beep.js
@@ -1,4 +1,5 @@
 /*
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,7 +8,7 @@
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -15,27 +16,27 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- */
-    if(navigator.userAgent.match(/Chrome|Trident/)){
+ *
+*/
 
-        console.log(""+navigator.userAgent);
-        onReady();
+module.exports = function (quantity) {
+    var count = 0,
+        beepObj,
+        play = function () { 
+            //create new object every time due to strage playback behaviour
+            beepObj = new Audio('local:///chrome/plugin/org.apache.cordova.dialogs/notification-beep.wav');
+            beepObj.addEventListener("ended", callback);
+            beepObj.play();
+        },
+        callback = function () {
+            if (--count > 0) {
+                play();
+            } else {
+                delete beepObj;
+            }
+        };
+    count += quantity || 1;
+    if (count > 0) {
+        play();
     }
-    else{
-        document.addEventListener("deviceready", onReady, false);
-    }
-
-    function onReady() {
-        // Handle the deviceready event.
-        initialize();
-    }
-
-    function initialize() {
-
-        var parentElement = document.getElementById("deviceready");
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-    }
+};
