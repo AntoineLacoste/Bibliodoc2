8cd75a00ca307fc28c9248b5fa3ec14cc7e9b88c
AntoineLacoste <antoine.lacoste@iut-valence.fr>
2015-05-05 09:05:35 +0200

    test device ready 3

--- www/js/index.js
+++ www/js/index.js
@@ -16,36 +16,26 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
+    if(navigator.userAgent.match(/Chrome|Trident/)){
+
+        console.log(""+navigator.userAgent);
+        onReady();
+    }
+    else{
+        document.addEventListener("deviceready", onReady, false);
+    }
+
+    function onReady() {
+        // Handle the deviceready event.
+        initialize();
+    }
+
+    function initialize() {
+
+        var parentElement = document.getElementById("deviceready");
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
     }
-};
-
-app.initialize();
